a0f70be906e76b150695e02dc2d6896baa485ff0
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-14 22:12:54 -0800

    starting work on legacy parser

--- src/hooks/plugins/parse/legacy.ts
+++ src/hooks/plugins/parse/legacy.ts
@@ -1,3 +1,17 @@
-module.exports = (...opts: any[]) => {
-  console.dir(opts)
+import {Config} from 'cli-engine-config'
+import {PluginsParseHookOptions} from 'cli-engine/lib/hooks'
+
+function getID (c: any): string {
+  let id = []
+  if (c.topic) id.push(c.topic)
+  if (c.command) id.push(c.command)
+  return id.join(':')
+}
+
+module.exports = (_: Config, opts: PluginsParseHookOptions) => {
+  const m = opts.module
+  m.commands = m.commands.map((c: any) => {
+    if (!c.id) c.id = getID(c)
+    return c
+  })
 }
