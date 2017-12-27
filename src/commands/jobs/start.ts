44e63ca22f35e5e4c883ed3ca8a1a4aa958e6c79
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-26 20:19:55 -0800

    lint

--- src/commands/jobs/start.ts
+++ src/commands/jobs/start.ts
@@ -4,7 +4,7 @@ import cli from 'cli-ux'
 export default class JobsStart extends Command {
   static description = 'this is the command description'
   static aliases = ['start', 'newjob']
-  static args = [{name: 'job', required: true}]
+  static args = [{ name: 'job', required: true }]
   static help = `
 this is some example help text
 `
